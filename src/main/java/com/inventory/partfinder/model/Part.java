7dfff8d3d0a3e651c5cd8a895d81174181089b66
Jonathan Velasquez <johedvelca@gmail.com>
2025-07-16 19:03:48 -0400

    Part entity created inside of new package named model

---
+++ src/main/java/com/inventory/partfinder/model/Part.java
@@ -0,0 +1,24 @@
+package com.inventory.partfinder.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Part {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String sku;
+    private int qty;
+    private String location;
+}
