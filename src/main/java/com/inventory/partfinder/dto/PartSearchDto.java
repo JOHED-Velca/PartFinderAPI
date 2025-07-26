4698db4aea5087de0e30c234d8cb7874b8b21e0e
Jonathan Velasquez <johedvelca@gmail.com>
2025-07-25 20:49:48 -0400

    replaced manual getters with Lombok annotations

--- src/main/java/com/inventory/partfinder/dto/PartSearchDto.java
+++ src/main/java/com/inventory/partfinder/dto/PartSearchDto.java
@@ -1,5 +1,10 @@
 package com.inventory.partfinder.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class PartSearchDto {
     private Long id;
     private String name;
@@ -9,22 +14,22 @@ public class PartSearchDto {
     private String side;
     private Integer level;
 
-    public PartSearchDto(Long id, String name, String sku, Integer quantity, Integer aisle, String side, Integer level) {
-        this.id = id;
-        this.name = name;
-        this.sku = sku;
-        this.quantity = quantity;
-        this.aisle = aisle;
-        this.side = side;
-        this.level = level;
-    }
-
-    //Getters only (optional to add setters)
-    public Long getId() { return id; }
-    public String getName() { return name; }
-    public String getSku() { return sku; }
-    public Integer getQuantity() { return quantity; }
-    public Integer getAisle() { return  aisle; }
-    public String getSide() { return side; }
-    public Integer getLevel() { return level; }
+//    public PartSearchDto(Long id, String name, String sku, Integer quantity, Integer aisle, String side, Integer level) {
+//        this.id = id;
+//        this.name = name;
+//        this.sku = sku;
+//        this.quantity = quantity;
+//        this.aisle = aisle;
+//        this.side = side;
+//        this.level = level;
+//    }
+//
+//    //Getters only (optional to add setters)
+//    public Long getId() { return id; }
+//    public String getName() { return name; }
+//    public String getSku() { return sku; }
+//    public Integer getQuantity() { return quantity; }
+//    public Integer getAisle() { return  aisle; }
+//    public String getSide() { return side; }
+//    public Integer getLevel() { return level; }
 }
